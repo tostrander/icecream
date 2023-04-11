55c3b0112b282e94560325fcdc3292698872dacb
Tina Ostrander <tostrander@greenriver.edu>
2023-04-11 13:43:04 -0400

    Add header include and order summary

---
+++ process.php
@@ -0,0 +1,61 @@
+<?php
+/*
+ * Tina Ostrander
+ * 4/11/2023
+ * 328/icecream/process.php
+ * Process Order for Ice Cream Shoppe
+
+  POST array(3) {
+  ["flavor"]=>array(2) {
+    [0]=>
+    string(9) "chocolate"
+    [1]=>
+    string(10) "strawberry"
+  }
+  ["cone"]=>string(6) "waffle"
+  ["scoops"]=>string(1) "7"
+}
+ */
+
+// Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+// Define constants
+define('PRICE_PER_SCOOP', 2.00);
+define('SALES_TAX', 0.08);
+
+// Include header
+include ('header.html');
+
+?>
+
+<body>
+<div class="container">
+
+    <h1>Thank you for your order!</h1>
+    <?php
+        //echo "<pre>";
+        //var_dump($_POST);
+        //echo "</pre>";
+
+        // Get data from POST array
+        $numScoops = $_POST['scoops'];
+        $cone = $_POST['cone'];
+        $flavors = $_POST['flavor'];
+        $flavorList = implode(", ", $flavors);
+
+        // Calculate price
+        $subtotal = $numScoops * PRICE_PER_SCOOP;
+        $total = $subtotal + ($subtotal * SALES_TAX);
+
+        // Display summary
+        echo "<p>Number of scoops: $numScoops</p>";
+        echo "<p>Cone selection: $cone</p>";
+        echo "<p>Flavors: $flavorList</p>";
+        echo "<p>Subtotal: $" . number_format($subtotal, 2) . "</p>";
+        echo "<p>Total: $" . number_format($total, 2) . "</p>";
+
+    ?>
+</div>
+</body>
